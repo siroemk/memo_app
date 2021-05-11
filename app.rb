731cb02978d09d71720c2c98c9434e6f6425a2cf
emiko <31835314+siroemk@users.noreply.github.com>
2021-05-11 13:52:31 +0900

    Gemfie、public、viewsディレクトリの追加

--- app.rb
+++ app.rb
@@ -1,1 +1,22 @@
 require 'sinatra'
+require 'sinatra/reloader'
+
+get '/memos' do
+  erb :index
+end
+
+get '/memos/new' do
+  erb :new
+end
+
+get '/memos/id' do
+  @title = 'タイトルです'
+  @content = '内容です'
+  erb :detail
+end
+
+get '/memos/edit' do
+  @title = 'タイトルです'
+  @content = '内容です'
+  erb :edit
+end
