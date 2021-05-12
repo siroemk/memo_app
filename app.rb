437b72b1c2d7f652cad400760c722dbc9e0d90bc
emiko <31835314+siroemk@users.noreply.github.com>
2021-05-12 13:28:29 +0900

    新規メモが作成されたらJSONファイルに保存する

--- app.rb
+++ app.rb
@@ -1,22 +1,30 @@
 require 'sinatra'
 require 'sinatra/reloader'
+require 'json'
 
 get '/memos' do
   erb :index
+end
+
+post '/memos' do
+  @title = params[:title]
+  @content = params[:content]
+
+  memo = { "id" => SecureRandom.uuid, "title" => @title, "content"=> @content }
+  File.open("data/memos_#{memo["id"]}.json", 'w') do |file|
+    JSON.dump(memo, file)
+  end
+  erb :detail
 end
 
 get '/memos/new' do
   erb :new
 end
 
-get '/memos/id' do
-  @title = 'タイトルです'
-  @content = '内容です'
+get '/memos/:id' do
   erb :detail
 end
 
-get '/memos/edit' do
-  @title = 'タイトルです'
-  @content = '内容です'
+get '/memos/:id/edit' do
   erb :edit
 end
