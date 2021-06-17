3820d63e64696fa7b702441bd3a7d5ed6b66b41a
emiko <31835314+siroemk@users.noreply.github.com>
2021-06-17 13:53:55 +0900

    dbに保存、削除、編集できるようにした

--- app.rb
+++ app.rb
@@ -2,15 +2,10 @@
 
 require 'sinatra'
 require 'sinatra/reloader'
-require_relative 'memo'
 
 helpers do
   def h(text)
     Rack::Utils.escape_html(text)
-  end
-
-  def memo
-    Memo.new
   end
 end
 
@@ -19,13 +14,18 @@ get '/' do
 end
 
 get '/memos' do
-
+  connection = PG.connect(dbname: 'memos')
+  @memos = connection.exec('SELECT * FROM t_memos')
   erb :index
 end
 
 post '/memos' do
-
-  redirect to("/memos/:id")
+  connection = PG.connect(dbname: 'memos')
+  title = params[:title]
+  content = params[:content]
+  memo = connection.exec('INSERT INTO t_memos (title, content) VALUES ($1, $2) RETURNING id' , [title, content])
+  id = memo.first['id']
+  redirect to("/memos/#{id}")
 end
 
 get '/memos/new' do
@@ -33,22 +33,37 @@ get '/memos/new' do
 end
 
 get '/memos/:id' do
-
+  connection = PG.connect(dbname: 'memos')
+  id = params[:id]
+  @memo = connection.exec('SELECT * FROM t_memos WHERE id = $1', [id]).first
+  @id = params[:id]
   erb :detail
 end
 
 get '/memos/:id/edit' do
-
+  connection = PG.connect(dbname: 'memos')
+  id = params[:id]
+  memo = connection.exec('SELECT * FROM t_memos WHERE id = $1', [id]).first
+  @title = memo['title']
+  @content = memo['content']
+  @id = params[:id]
   erb :edit
 end
 
 patch '/memos/:id/edit' do
-
-  redirect to("/memos/#{params[:id]}")
+  connection = PG.connect(dbname: 'memos')
+  id = params[:id]
+  title = params[:title]
+  content = params[:content]
+  @id = params[:id]
+  connection.exec('UPDATE t_memos SET title = $1, content = $2 WHERE id = $3', [title, content, id])
+  redirect to("/memos/#{id}")
 end
 
 delete '/memos/:id' do
-
+  connection = PG.connect(dbname: 'memos')
+  id = params[:id]
+  connection.exec('DELETE FROM t_memos WHERE id = $1', [id])
   redirect to('/memos')
 end
 
